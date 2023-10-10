354d0c93287fda7bb14128a77c69828d23542643
Donato Silva <figueroajesus2015@hotmail.com>
2023-10-10 10:45:00 -0500

    Se creo la clase ConnectionDB donde se crea la coneccion y se verica si la db existe y si no la crea

--- ConnectionDB.cs
+++ ConnectionDB.cs
@@ -10,7 +10,7 @@ namespace Fotolva
         // The data for the connection is defined
         private static readonly string server = "localhost";
         private static readonly string port = "3306";
-        private static readonly string user = "rot";
+        private static readonly string user = "ro0t";
         private static readonly string password = "";
 
         // The data for the db is defined
@@ -19,20 +19,62 @@ namespace Fotolva
         private static readonly string connectionString = $"server={server};port={port};user={user};password={password};";
 
         // the connection returns
-        public MySqlConnection EstablishConnection()
+        private MySqlConnection EstablishConnection()
         {
             MySqlConnection conn = null;
 
             try
             {
                 conn = new MySqlConnection(connectionString);
-            } 
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error al realizar la conexion, mensaje de error: " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return conn;
+        }
+
+        // the connection the DBFotolva returns
+        public MySqlConnection EstablishConnectionToDB()
+        {   
+            EnsureDatabaseExists();
+
+            MySqlConnection conn = null;
+
+            try
+            {
+                string deConnectionString = $"{connectionString}detabase={nameDB};";
+                conn = new MySqlConnection(deConnectionString);
+            }
             catch (Exception e)
             {
                 MessageBox.Show("Error al conectar con la base de datos, mensaje de error: " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
             return conn;
+        }
+
+        //Create table if it does not exist
+        private void EnsureDatabaseExists()
+        {
+            MySqlConnection conn = EstablishConnection();
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand(); 
+                cmd.CommandText = $"CREATE DATABASE IF NOT EXISTS {nameDB};"; // Prepare the query
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error al crear la base de datos, mensaje de error: " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
