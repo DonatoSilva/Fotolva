f585a6eb3b84f66b413158ed52d551c550b6964a
Donato Silva <figueroajesus2015@hotmail.com>
2023-10-11 15:18:26 -0500

    como unicio de la apariencia se le agrego un titulo a la vista acompañada de una imagen (icono) y el boton nuevo que esta customizado

---
+++ Custom/ReundedButton.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Fotolva.Custom
+{
+    public class ReundedButton : Button
+    {
+        private int borderSize = 0;
+        private int _borderRadius = 40;
+        private Color borderColor = Color.FromArgb(169, 143, 118);
+
+        public int BorderSize { get => borderSize; set { 
+                borderSize = value; this.Invalidate(); 
+            } }
+        public int BorderRadius { get => _borderRadius; set { 
+                _borderRadius = value; this.Invalidate(); 
+            } }
+        public Color BorderColor { get => borderColor; set {
+                borderColor = value;
+                this.Invalidate();
+            } }
+
+        // Constructor de la clase
+        public ReundedButton()
+        {
+            // Establecer el estilo del botón a plano
+            this.FlatStyle = FlatStyle.Flat;
+            // Establecer el grosor del borde a cero
+            this.FlatAppearance.BorderSize = 0;
+            // Establecer el color de fondo a transparente
+            this.BackColor = Color.Transparent;
+            this.ForeColor = Color.White;
+        }
+
+        // Método para dibujar el botón con bordes redondeados
+        private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+        {
+            // Crear un objeto GraphicsPath para dibujar el borde redondeado
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            // Añadir un arco en cada esquina del rectángulo con el radio de borde especificado
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.X + rect.Width -  radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.X + rect.Width - radius, rect.Y + rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Y + rect.Height - radius, radius, radius, 90, 90);
+            // Cerrar el camino
+            path.CloseAllFigures();
+
+            return path;            
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RectangleF rectSurface = new RectangleF(0,0, this.Width, this.Height);
+            RectangleF rectBorder = new RectangleF(1,1,this.Width-0.8F,this.Height-1);
+
+            if (_borderRadius > 2)
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, _borderRadius))
+                using (GraphicsPath parhBorder = GetFigurePath(rectBorder, _borderRadius - 1F))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, 2))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    penBorder.Alignment = PenAlignment.Inset;
+                    this.Region = new Region(pathSurface);
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+
+                    if (borderSize >= 1)
+                    {
+                        pevent.Graphics.DrawPath(penBorder, parhBorder);
+                    }
+                    
+                }
+            }
+            else
+            {
+                this.Region = new Region(rectSurface);
+                if (borderSize >= 1)
+                {
+                    using (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        pevent.Graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
+                    }
+                }
+                
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Container_BackColorChange);
+        }
+
+        private void Container_BackColorChange(object sender, EventArgs e)
+        {
+            if (this.DesignMode)
+            {
+                this.Invalidate();
+            }
+        }
+    }
+}
--- Home.Designer.cs
+++ Home.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.TitleHome = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.reundedButton1 = new Fotolva.Custom.ReundedButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TitleHome
+            // 
+            this.TitleHome.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.TitleHome.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleHome.Location = new System.Drawing.Point(10, 10);
+            this.TitleHome.Margin = new System.Windows.Forms.Padding(1);
+            this.TitleHome.Name = "TitleHome";
+            this.TitleHome.Size = new System.Drawing.Size(88, 39);
+            this.TitleHome.TabIndex = 0;
+            this.TitleHome.Text = "Fotolva";
+            this.TitleHome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Fotolva.Properties.Resources.icon;
+            this.pictureBox1.Location = new System.Drawing.Point(97, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // reundedButton1
+            // 
+            this.reundedButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(164)))), ((int)(((byte)(211)))));
+            this.reundedButton1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(164)))), ((int)(((byte)(211)))));
+            this.reundedButton1.BorderRadius = 20;
+            this.reundedButton1.BorderSize = 0;
+            this.reundedButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.reundedButton1.FlatAppearance.BorderSize = 0;
+            this.reundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reundedButton1.Font = new System.Drawing.Font("Segoe UI Variable Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reundedButton1.ForeColor = System.Drawing.Color.White;
+            this.reundedButton1.Image = global::Fotolva.Properties.Resources.plaza_mas;
+            this.reundedButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.reundedButton1.Location = new System.Drawing.Point(370, 10);
+            this.reundedButton1.Name = "reundedButton1";
+            this.reundedButton1.Size = new System.Drawing.Size(100, 39);
+            this.reundedButton1.TabIndex = 3;
+            this.reundedButton1.Text = "Nuevo";
+            this.reundedButton1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.reundedButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.reundedButton1.UseVisualStyleBackColor = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(482, 653);
+            this.Controls.Add(this.reundedButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TitleHome);
+            this.Font = new System.Drawing.Font("Segoe UI Variable Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fotolva - Home";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TitleHome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Custom.ReundedButton reundedButton1;
     }
 }
 
